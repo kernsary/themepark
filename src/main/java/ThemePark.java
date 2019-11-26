f0475d31d7e0c6762ce40ac379fa26c668df15d4
Charles Kennedy <charlesscottkennedy@googlemail.com>
2019-11-26 22:55:08 +0000

    added IReviewed array

--- src/main/java/ThemePark.java
+++ src/main/java/ThemePark.java
@@ -1,2 +1,43 @@
+import attractions.Dodgems;
+import attractions.Park;
+import attractions.Playground;
+import attractions.RollerCoaster;
+import behaviours.IReviewed;
+import stalls.CandyflossStall;
+import stalls.IceCreamStall;
+import stalls.ParkingSpot;
+import stalls.TobaccoStall;
+
+import java.util.ArrayList;
+
 public class ThemePark {
+
+//    private Dodgems dodgems;
+//    private Park park;
+//    private Playground playground;
+//    private RollerCoaster rollerCoaster;
+//    private CandyflossStall candyflossStall;
+//    private IceCreamStall iceCreamStall;
+//    private TobaccoStall tobaccoStall;tobaccoStall
+    private ArrayList<IReviewed> reviewedItems;
+
+    public ThemePark(){
+//        this.dodgems = new Dodgems("Crunk", 6);
+//        this.park = new Park("Birdsong", 9);
+//        this.playground = new Playground("Mania", 4);
+//        this.rollerCoaster = new RollerCoaster("Whitey", 7);
+//        this.candyflossStall = new CandyflossStall("GooMaster", "Orville Fillingsgone", ParkingSpot.A1, 3);
+//        this.iceCreamStall = new IceCreamStall("PokyHats", "Icy Izzy", ParkingSpot.A4, 8);
+//        this.tobaccoStall = new TobaccoStall("LungBusters", "Horace Hoarse", ParkingSpot.A2, 5);
+        this.reviewedItems = new ArrayList<IReviewed>();
+    }
+
+    public void addItem(IReviewed item){
+        reviewedItems.add(item);
+    }
+
+    public ArrayList<IReviewed>getAllReviewed(){
+        return reviewedItems;
+    }
+
 }
---
+++ src/test/java/ThemeParkTest.java
@@ -0,0 +1,48 @@
+import attractions.Dodgems;
+import attractions.Park;
+import attractions.Playground;
+import attractions.RollerCoaster;
+import org.junit.Before;
+import org.junit.Test;
+import stalls.CandyflossStall;
+import stalls.IceCreamStall;
+import stalls.ParkingSpot;
+import stalls.TobaccoStall;
+
+import static org.junit.Assert.assertEquals;
+
+public class ThemeParkTest {
+
+    private ThemePark themePark;
+    private Dodgems dodgems;
+    private Park park;
+    private Playground playground;
+    private RollerCoaster rollerCoaster;
+    private CandyflossStall candyflossStall;
+    private IceCreamStall iceCreamStall;
+    private TobaccoStall tobaccoStall;
+
+    @Before
+    public void before(){
+        themePark = new ThemePark();
+        dodgems = new Dodgems("Crunk", 6);
+        park = new Park("Birdsong", 9);
+        playground = new Playground("Mania", 4);
+        rollerCoaster = new RollerCoaster("Whitey", 7);
+        candyflossStall = new CandyflossStall("GooMaster", "Orville Fillingsgone", ParkingSpot.A1, 3);
+        iceCreamStall = new IceCreamStall("PokyHats", "Icy Izzy", ParkingSpot.A4, 8);
+        tobaccoStall = new TobaccoStall("LungBusters", "Horace Hoarse", ParkingSpot.A2, 5);
+        themePark.addItem(dodgems);
+        themePark.addItem(park);
+        themePark.addItem(playground);
+        themePark.addItem(rollerCoaster);
+        themePark.addItem(candyflossStall);
+        themePark.addItem(iceCreamStall);
+        themePark.addItem(tobaccoStall);
+    }
+
+    @Test
+    public void hasReviewedArray(){
+        assertEquals(7, themePark.getAllReviewed().size());
+    }
+}
